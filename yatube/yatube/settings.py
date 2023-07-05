4b00af830d753cea5b7c22f392d5cda2ea210ecb
Мария Григорьева <m.grigoryeva@inbox.ru>
2023-07-05 20:45:05 +0300

    settings for debloy

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -11,6 +11,8 @@ ALLOWED_HOSTS = [
     '127.0.0.1',
     '[::1]',
     'testserver',
+    'www.mariagrigoryeva.pythonanywhere.com',
+    'mariagrigoryeva.pythonanywhere.com',
 ]
 
 INSTALLED_APPS = [
@@ -23,6 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'debug_toolbar',
     'sorl.thumbnail',
 ]
 
@@ -34,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 ROOT_URLCONF = 'yatube.urls'
 
@@ -113,3 +117,8 @@ CACHES = {
         'BACKEND': 'django.core.cache.backends.locmem.LocMemCache',
     }
 }
+
+
+INTERNAL_IPS = [
+    '127.0.0.1',
+] 
